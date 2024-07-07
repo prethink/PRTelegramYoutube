2f2627f0130e41f54331a48c65ebdc2370df37f2
PreThink <ilia-samarin@bk.ru>
2024-07-07 14:41:26 +0700

    Добавьте файлы проекта.

---
+++ PRTelegramYoutube/Commands.cs
@@ -0,0 +1,16 @@
+﻿using PRTelegramBot.Attributes;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace PRTelegramYoutube
+{
+    internal class Commands
+    {
+        [ReplyMenuHandler("Тест")]
+        public static async Task TestMethod(ITelegramBotClient botclient, Update update)
+        {
+            string msg = "Hello World";
+            await PRTelegramBot.Helpers.Message.Send(botclient, update, msg);
+        }
+    }
+}
---
+++ PRTelegramYoutube/Program.cs
@@ -0,0 +1,15 @@
+﻿using PRTelegramBot.Core;
+
+var bot = new PRBotBuilder("Token").Build();
+bot.Events.OnCommonLog += Events_OnCommonLog;
+_ = bot.Start();
+
+async Task Events_OnCommonLog(PRTelegramBot.Models.EventsArgs.CommonLogEventArgs arg)
+{
+    Console.WriteLine(arg.Message);
+}
+
+while (true)
+{
+
+}
