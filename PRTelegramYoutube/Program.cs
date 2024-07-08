da41e0590bf9e7dd222417b0b86ea93a25bf56ae
PreThink <ilia-samarin@bk.ru>
2024-07-08 13:01:13 +0700

    Урок 4. Работа с несколькими ботами

--- PRTelegramYoutube/Program.cs
+++ PRTelegramYoutube/Program.cs
@@ -6,12 +6,24 @@ var botProviders = new BotConfigJsonProvider(".\\Configs\\commands.json");
 var dynamicCommands = botProviders.GetKeysAndValues();
 
 var bot = new PRBotBuilder("Token")
+    .SetBotId(0)
     .AddReplyDynamicCommands(dynamicCommands)
     .AddConfigPath("Message",".//Configs//message.json")
     .Build();
 
+var botTwo = new PRBotBuilder("Token")
+    .SetBotId(1)
+    .AddReplyDynamicCommands(dynamicCommands)
+    .AddConfigPath("Message", ".//Configs//message.json")
+    .Build();
+
+
 bot.Events.OnCommonLog += Events_OnCommonLog;
+botTwo.Events.OnCommonLog += Events_OnCommonLog;
 _ = bot.Start();
+_ = botTwo.Start();
+
+var bots = BotCollection.Instance.GetBots();
 
 async Task Events_OnCommonLog(CommonLogEventArgs arg)
 {
