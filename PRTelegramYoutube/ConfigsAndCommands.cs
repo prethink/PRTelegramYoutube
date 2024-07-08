3aca80a86a81b5e3c91b40dae02431ab70fda323
PreThink <ilia-samarin@bk.ru>
2024-07-08 10:54:12 +0700

    Урок 3 - конфигурационные файлы и динамические команды

---
+++ PRTelegramYoutube/ConfigsAndCommands.cs
@@ -0,0 +1,18 @@
+﻿using PRTelegramBot.Attributes;
+using PRTelegramBot.Configs;
+using PRTelegramBot.Extensions;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace PRTelegramYoutube
+{
+    internal class ConfigsAndCommands
+    {
+        [ReplyMenuDynamicHandler("START")]
+        public static async Task ReplyDynamic(ITelegramBotClient botClient, Update update)
+        {
+            string msg = botClient.GetConfigValue<BotConfigJsonProvider, string>("Message", "MSG");
+            await PRTelegramBot.Helpers.Message.Send(botClient, update, msg);
+        }
+    }
+}
