26b71e41ee0931222ce832c443354a353f079780
PreThink <ilia-samarin@bk.ru>
2024-07-14 15:53:40 +0700

    Урок 6 - работа с администраторами

---
+++ PRTelegramYoutube/Admins/AdminCommands.cs
@@ -0,0 +1,25 @@
+﻿using PRTelegramBot.Attributes;
+using PRTelegramBot.Extensions;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace PRTelegramYoutube.Admins
+{
+    internal class AdminCommands
+    {
+        [ReplyMenuHandler("Admin")]
+        public static async Task AdminTest(ITelegramBotClient botClient, Update update)
+        {
+            var isAdmin = await botClient.IsAdmin(update);
+            isAdmin = await botClient.GetBotDataOrNull().Options.AdminManager.HasUser(update.GetChatId());
+            if(isAdmin)
+            {
+                await botClient.SendTextMessageAsync(update.GetChatIdClass(), "Я админ");
+            }
+            else
+            {
+                await botClient.SendTextMessageAsync(update.GetChatIdClass(), "Я не админ");
+            }
+        }
+    }
+}
