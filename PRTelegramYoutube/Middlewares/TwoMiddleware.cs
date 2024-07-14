9b22e2f790f31d9b624b0b41b7e1d06964786364
PreThink <ilia-samarin@bk.ru>
2024-07-14 14:10:03 +0700

    5 Урок middlewares

---
+++ PRTelegramYoutube/Middlewares/TwoMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using PRTelegramBot.Core.Middlewares;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+
+namespace PRTelegramYoutube.Middlewares
+{
+    internal class TwoMiddleware : MiddlewareBase
+    {
+        public override async Task InvokeOnPreUpdateAsync(ITelegramBotClient botClient, Update update, Func<Task> next)
+        {
+            Console.WriteLine("Второй обработчик до");
+            await base.InvokeOnPreUpdateAsync(botClient, update, next);
+        }
+
+        public override async Task InvokeOnPostUpdateAsync(ITelegramBotClient botClient, Update update)
+        {
+            Console.WriteLine("Второй обработчик после");
+            await base.InvokeOnPostUpdateAsync(botClient, update);
+        }
+    }
+}
--- PRTelegramYoutube/Program.cs
+++ PRTelegramYoutube/Program.cs
@@ -1,6 +1,7 @@
 ﻿using PRTelegramBot.Configs;
 using PRTelegramBot.Core;
 using PRTelegramBot.Models.EventsArgs;
+using PRTelegramYoutube.Middlewares;
 
 var botProviders = new BotConfigJsonProvider(".\\Configs\\commands.json");
 var dynamicCommands = botProviders.GetKeysAndValues();
@@ -9,19 +10,12 @@ var bot = new PRBotBuilder("Token")
     .SetBotId(0)
     .AddReplyDynamicCommands(dynamicCommands)
     .AddConfigPath("Message",".//Configs//message.json")
-    .Build();
-
-var botTwo = new PRBotBuilder("Token")
-    .SetBotId(1)
-    .AddReplyDynamicCommands(dynamicCommands)
-    .AddConfigPath("Message", ".//Configs//message.json")
+    .AddMiddlewares(new OneMiddleware(), new ThreeMiddleware(), new TwoMiddleware())
     .Build();
 
 
 bot.Events.OnCommonLog += Events_OnCommonLog;
-botTwo.Events.OnCommonLog += Events_OnCommonLog;
 _ = bot.Start();
-_ = botTwo.Start();
 
 var bots = BotCollection.Instance.GetBots();
 
